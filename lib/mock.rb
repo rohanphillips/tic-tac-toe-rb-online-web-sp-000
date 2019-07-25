55273514a5c695af94a6bc7e096b830bd1d78a17
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-25 16:24:12 +0000

    Automatically backed up by Learn

--- lib/mock.rb
+++ lib/mock.rb
@@ -17,97 +17,19 @@ $winningcombo = -1
 
 
 def won?(board)
+  #for the purpose of this study, winner(board) returns nil or "X" or "O"
   result = winner(board)
   if result == nil 
+    #if the result of winner is null, return false
     return false  
   end
-  return WIN_COMBINATIONS[$winningcombo]
+  #return the array at location 0
+  return WIN_COMBINATIONS[0]
 end
 
 def play(board)
+  #i'm attempting to assign the return of the won? method to result
   result = won?(board)
-  
-  if isover == false 
-    puts "Welcome to Tic Tac Toe!"
-    display_board(board)
-    while isover == false
-      puts "It's player #{current_player(board)}'s turn" 
-      turn(board)
-      result = won?(board)
-      isover = over?(board)
-    end
-  end
-  
-  if isover
-    
-    
-    if result
-      newresult = won?(board)
-      puts newresult.class
-      binding.pry
-      firstlocation = newresult[0]
-     # puts firstlocation
-      #puts "Congratulations #{board[firstlocation]}!"
-    end
-  end
-    
-  
-end
-
-def winner(board)
-  winnerfound = false 
-  PLAYERS.each do |i|
-    if iswinner(board, i) == i 
-      return i 
-      winnerfound = true
-      break
-    end
-  end
-  if winnerfound == false
-    return nil  
-  end
-end
-
-def iswinner(board, player)
-  winfound = false 
-  complete = false
-  combo = []
-  combinationcounter = 0
-   
-  until complete == true 
-    while combinationcounter < WIN_COMBINATIONS.length 
-      combo = WIN_COMBINATIONS[combinationcounter]
-      combocounter = 0
-      playercounter = 0
-      while combocounter < combo.length
-       boardcontent = board[combo[combocounter]]
-        if boardcontent == player
-          playercounter += 1  
-        end 
-        combocounter += 1
-      end
-      if playercounter == 3
-        winfound = true 
-        complete = true
-        $winningcombo = combinationcounter
-        break
-      end
-      combinationcounter += 1
-    end
-    complete = true 
-  end
-  if winfound == true 
-    return player  
-  end
-end
-
-def full?(board)
-  isfull = true 
-  board.each do |location|
-    if location == " "
-      isfull = false 
-      break 
-    end
-  end
-  return isfull
+  #when result is evaluated, when an array is being returned, result becomes true or false,
+  #rather than what I'd expect as false or an array
 end
